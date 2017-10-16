2634f9a875fe10438cb4b8a26f4411dd7348a32a
jason.davis@ncm.com <jd@nonviolence.us>
2017-10-16 08:07:15 -0600

    escape message if a string before building sql insert statement.

--- mySQLHandler.py
+++ mySQLHandler.py
@@ -7,6 +7,7 @@ import MySQLdb
 import _mysql_exceptions
 import logging
 import time
+import six
  
 class mySQLHandler(logging.Handler):
     """
@@ -165,7 +166,6 @@ class mySQLHandler(logging.Handler):
         else:
             record.exc_text = ""
         # Insert log record:
-        sql = mySQLHandler.insertion_sql % record.__dict__
         try:
             conn=MySQLdb.connect(host=self.db['host'],port=self.db['port'],user=self.db['dbuser'],passwd=self.db['dbpassword'],db=self.db['dbname'])
         except _mysql_exceptions, e:
@@ -174,6 +174,10 @@ class mySQLHandler(logging.Handler):
             pprint(e)
             raise Exception(e)
             exit(-1)
+        # escape the message to allow for SQL special chars
+        if isinstance(record.msg, six.string_types):# check is a string
+          record.msg=conn.escape_string(record.msg)
+        sql = mySQLHandler.insertion_sql % record.__dict__
         cur = conn.cursor()
         try:
             cur.execute(sql)
